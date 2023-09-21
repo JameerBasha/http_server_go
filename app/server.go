5fda7a90abf44e8678b240c443c730d8b2d312e0
Jameerbasha J <jameerbasha14@gmail.com>
2023-09-21 08:52:01 +0400

    Dummy commit for concurrent connections

--- app/server.go
+++ app/server.go
@@ -89,7 +89,7 @@ func main() {
 
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
-		fmt.Println("Failed to bind to port 4221")
+		fmt.Println("Failed to bind to port :4221")
 		os.Exit(1)
 	}
 	for {
