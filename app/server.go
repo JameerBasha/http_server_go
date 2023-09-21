8325d7596c9f50288561f2d997196f2ece9bc6df
Jameerbasha J <jameerbasha14@gmail.com>
2023-09-21 06:43:34 +0400

    Added 404 response

--- app/server.go
+++ app/server.go
@@ -3,25 +3,34 @@ package main
 import (
 	"fmt"
 	"log"
+	"strings"
 
 	// Uncomment this block to pass the first stage
 	"net"
 	"os"
 )
 
+func sendResponseAndCloseConnection(conn net.Conn, content string) {
+	conn.Write([]byte(content))
+	conn.Close()
+}
+
 func processRequest(conn net.Conn) {
 	buff := make([]byte, 1024)
 	_, err := conn.Read(buff)
+	requestContent := strings.Split(string(buff[:]), " ")
+	if requestContent[0] != "GET" || requestContent[1] != "/" {
+		sendResponseAndCloseConnection(conn, "HTTP/1.1 404\r\n\r\n")
+		return
+	}
 	if err != nil {
 		log.Fatal(err)
 	}
-	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-	conn.Close()
+	sendResponseAndCloseConnection(conn, "HTTP/1.1 200 OK\r\n\r\n")
 }
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
 
 	// Uncomment this block to pass the first stage
 
