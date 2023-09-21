91690f5dfcc8cb483c6a09a87e5812336794490f
Jameerbasha J <jameerbasha14@gmail.com>
2023-09-21 08:21:14 +0400

    Added response for user-agent

--- app/server.go
+++ app/server.go
@@ -15,17 +15,58 @@ func sendResponseAndCloseConnection(conn net.Conn, content string) {
 	conn.Close()
 }
 
+func getContentLength(content string) string {
+	return fmt.Sprint(len(content))
+}
+
+func sendResponseWithContent(content string, conn net.Conn) {
+	response := "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + getContentLength(content) + "\r\n\r\n" + content
+	sendResponseAndCloseConnection(conn, response)
+}
+
+type ownHeader struct {
+	header string
+	value  string
+}
+
+func processHeaders(content string) []ownHeader {
+	requestBody := strings.Split(content, "\n")
+	var headers []ownHeader
+	for i := 1; i < len(requestBody); i++ {
+		currentHeader := strings.SplitN(requestBody[i], ":", 2)
+		if len(currentHeader) == 2 {
+			headers = append(headers, ownHeader{header: currentHeader[0], value: currentHeader[1][1:]})
+		}
+	}
+	return headers
+}
+
 func processRequest(conn net.Conn) {
 	buff := make([]byte, 1024)
 	_, err := conn.Read(buff)
 	requestContent := strings.Split(string(buff[:]), " ")
-	// fmt.Println(requestContent)
-	// fmt.Println(strings.Split(requestContent[1], "/")[1])
+	headers := processHeaders(string(buff[:]))
+	if requestContent[0] == "GET" && requestContent[1] == "/user-agent" {
+		for i := 0; i < len(headers); i++ {
+			if headers[i].header == "User-Agent" {
+				sendResponseWithContent(headers[i].value, conn)
+				return
+			}
+		}
+
+		for i := 0; i < len(requestContent); i++ {
+			currentString := strings.Split(requestContent[i], " ")
+			for j := 0; j < len(currentString); j++ {
+				if currentString[j] == "User-Agent:a" {
+					sendResponseWithContent(currentString[j], conn)
+					return
+				}
+			}
+		}
+	}
 	if requestContent[0] == "GET" && strings.Split(requestContent[1], "/")[1] == "echo" {
-		echoResponse := requestContent[1][6:] // strings.Join(strings.Split(requestContent[1], "GET")[1:], "")
-		fmt.Println(echoResponse)
-		response := "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + fmt.Sprint(len(echoResponse)) + "\r\n\r\n" + echoResponse //+ "\r\n"
-		sendResponseAndCloseConnection(conn, response)
+		echoResponse := requestContent[1][6:]
+		sendResponseWithContent(echoResponse, conn)
 		return
 	}
 	if requestContent[0] != "GET" || requestContent[1] != "/" {
